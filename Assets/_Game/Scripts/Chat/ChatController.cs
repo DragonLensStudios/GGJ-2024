532f56d0d05768526c71ddb52c6c3148844b6713
Stormi Dragon <Thegreatjinxycat@gmail.com>
2024-01-26 20:59:08 -0600

    Updated Scene, Added ChatMessage Basic Handling

--- Assets/_Game/Scripts/Chat/ChatController.cs
+++ Assets/_Game/Scripts/Chat/ChatController.cs
@@ -1,11 +1,21 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using Enums;
 using GEmojiSharp;
+using Messaging;
+using Messaging.Messages;
 using TMPro;
 using UnityEngine;
 
 public class ChatController : MonoBehaviour
 {
     [field: SerializeField] public List<ChatMessage> ChatMessages { get; set; } = new();
-    
+
+    private void Start()
+    {
+        var chatMessage = new ChatMessage("Todd", "I Like Eggs");
+        ChatMessages.Add(chatMessage);
+        MessageSystem.MessageManager.SendImmediate(MessageChannels.UI, new AddChatMessage(chatMessage.Sender, chatMessage.Message));
+    }
 }
